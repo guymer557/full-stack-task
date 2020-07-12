f1a317a545a68b1654c68cc3f6faf9e1afd2879d
Guy <guymer557@gmail.com>
2020-07-12 13:46:57 +0300

    refactor: changed component locations

--- client/src/components/nav-bar/NavBar.jsx
+++ client/src/components/nav-bar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import menuConfig from "../../menu-config"
-import Menu from "./menu/Menu";
+import Menu from "../menu/Menu";
 
 export default function NavBar() {
    return <Menu menuType="navBar" menuNodes={menuConfig}/>
